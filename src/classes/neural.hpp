6071d70fd8ec3661aec5ee6b786c0d817d8234b7
Curtis Lunn <145430426+lunncurt@users.noreply.github.com>
2024-05-31 21:26:10 -0400

    framework

---
+++ src/classes/neural.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <vector>
+
+class InputLayer{
+private:
+
+public:
+
+};
+
+class HiddenLayer{
+private:
+
+public:
+
+};
+
+class OutputLayer{
+private:
+
+public:
+
+};
