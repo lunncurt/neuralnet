553fd108dd4f8c61f1c151530291b3fb1a87e6b4
Curtis Lunn <145430426+lunncurt@users.noreply.github.com>
2024-09-29 05:06:56 -0400

    added first implementation of forward pass and reworked some basic structure/function definintions

--- src/classes/neural.cpp
+++ src/classes/neural.cpp
@@ -13,48 +13,50 @@ Layer::Layer(int input_size, int output_size, const char type)
 
   // fill the weights matrix
   weights =
-      Eigen::MatrixXd::Zero(input_size, output_size).unaryExpr([&](double x) {
+      Eigen::MatrixXd::Zero(output_size, input_size).unaryExpr([&](double x) {
         return he_dist(gen);
       });
 
   // Initialize bias vectors based on layer type
-  if (layer_type == 'i') {
-    biases = Eigen::VectorXd();
-  } else if (layer_type == 'h') {
-    biases = Eigen::VectorXd(weights.cols(), 0.1);
-  } else {
+  if (layer_type == 'h') {
+    biases = Eigen::VectorXd::Constant(weights.cols(), 0.1);
+  } else { // output layer
     biases = Eigen::VectorXd::Constant(weights.cols(), 0.0);
   }
 }
 
-Eigen::MatrixXd Layer::forward(const Eigen::MatrixXd &input) {
-  if (layer_type == 'i') {
-    return input;
-  } else {
-    // Calculate weighted sum + biases
-    Eigen::VectorXd wsum = (weights * input) + biases;
+Eigen::VectorXd Layer::forward(const Eigen::VectorXd &input) {
+  // Calculate weighted sum + biases
+  Eigen::VectorXd wsum = (weights * input) + biases;
 
-    // Apply each layers respective activation function
-    if (layer_type == 'h') {
-      // ReLU
-      output = wsum.unaryExpr([](double val) { return std::max(0.0, val); });
-    } else if (layer_type == 'o') {
-      // Softmax
-      Eigen::VectorXd smax = wsum.array().exp();
-      output = smax / smax.sum();
-    }
+  // Apply each layers respective activation function
+  if (layer_type == 'h') {
+    // ReLU
+    output = wsum.unaryExpr([](double val) { return std::max(0.0, val); });
+  } else if (layer_type == 'o') {
+    // Softmax
+    Eigen::VectorXd smax = (wsum.array() - wsum.maxCoeff()).exp();
+    output = smax / smax.sum();
   }
 
   return output;
 }
 
 Network::Network(const std::vector<int> &topology) {
-  layers.push_back(Layer(topology[0], topology[1], 'i'));
-
   for (size_t i = 1; i < topology.size() - 1; i++) {
     layers.push_back(Layer(topology[i], topology[i + 1], 'h'));
   }
 
   layers.push_back(
       Layer(topology[topology.size() - 2], topology[topology.size() - 1], 'o'));
+}
+
+Eigen::VectorXd Network::forward(const Eigen::VectorXd &input_data){
+  Eigen::VectorXd output = input_data;
+
+  for(auto &layer : layers){
+    output = layer.forward(output);
+  }
+
+  return output;
 }
--- src/classes/neural.hpp
+++ src/classes/neural.hpp
@@ -1,25 +1,28 @@
 #pragma once
 
 #include <Eigen/Dense>
-#include <functional>
 #include <vector>
+
+struct Values{
+  int label;
+  Eigen::VectorXd data;
+};
 
 class Layer {
 public:
   Eigen::MatrixXd weights;
   Eigen::VectorXd biases;
-  Eigen::MatrixXd activation;
   Eigen::VectorXd output;
 
   // Char value to determine layer type for bias initialization
-  // (i = input, h = hidden, o = output)
+  // (h = hidden, o = output)
   char layer_type;
 
   // Constructor
-  Layer(int input_size, int outputsize, char layer_type);
+  Layer(int input_size, int output_size, char layer_type);
 
   // Forward Pass: Layer output
-  Eigen::MatrixXd forward(const Eigen::MatrixXd &input);
+  Eigen::VectorXd forward(const Eigen::VectorXd &input);
 
   // Backward Pass: Update weights and biases
   Eigen::MatrixXd backward(const Eigen::MatrixXd &grad_output,
@@ -35,13 +38,13 @@ private:
 public:
   Network(const std::vector<int> &topology);
 
-  Eigen::MatrixXd forward(const Eigen::MatrixXd &input_batch);
+  Eigen::VectorXd forward(const Eigen::VectorXd &input_data);
 
   void backward(const Eigen::MatrixXd &loss_gradient, double learning_rate);
 
   void train(const Eigen::MatrixXd &input_batch, const Eigen::MatrixXd &labels,
              double learning_rate, int epochs);
 
-  double compute_loss(const Eigen::MatrixXd &output,
+  double compute_loss(const Eigen::VectorXd &output,
                       const Eigen::MatrixXd &labels);
 };
